a3bd9e0fbf7a5fcdacdd02e6907f3bc7a063bbd8
Ashish Vishwakarma (Robert Bosch Engineering) <v-ashvis@microsoft.com>
2017-08-04 13:16:19 +0530

    Pushing to GitHub

---
+++ Dynamics365ConsoleCaller/Connect.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.ServiceModel.Description;
+
+namespace Dynamics365ConsoleCaller
+{
+    public class Connect
+    {
+        public static IOrganizationService GetOrganizationService(string UserName, string Password, string SoapOrgServiceUri)
+        {
+            try
+            {
+                ClientCredentials credentials = new ClientCredentials();
+                credentials.UserName.UserName = UserName;
+                credentials.UserName.Password = Password;
+                Uri serviceUri = new Uri(SoapOrgServiceUri);
+                OrganizationServiceProxy proxy = new OrganizationServiceProxy(serviceUri, null, credentials, null);
+                proxy.EnableProxyTypes();
+                return (IOrganizationService)proxy;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while connecting to CRM " + ex.Message);
+                Console.ReadKey();
+                return null;
+            }
+        }
+    }
+}
---
+++ Dynamics365ConsoleCaller/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dynamics365ConsoleCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrganizationService service = Connect.GetOrganizationService("UID", "PWD", "https://org.api.crm.dynamics.com/XRMServices/2011/Organization.svc");
+            Console.WriteLine("Connected to Organization Service!");
+        }
+    }
+}
