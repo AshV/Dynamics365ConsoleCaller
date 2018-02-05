8834d95bfb19de6d8a63a02541ea2a8bb7b96778
AshV <ashish6318@gmail.com>
2018-02-06 00:36:06 +0530

    Updates DLLs to V9.0

---
+++ Dynamics365ConsoleCaller/Dynamics365ConsoleCaller/Connect.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Dynamics365ConsoleCaller
+{
+    public class Connect
+    {
+        static bool isFileTraceLogOn = false;
+        static string filePath = "";
+
+        static Connect()
+        {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+        }
+
+        public static IOrganizationService GetOrganizationService(string userName, string password, string loginUri)
+        {
+            try
+            {
+                var conn = new CrmServiceClient($@"Url={loginUri}; Username={userName}; Password={password}; authtype=Office365");
+
+                // Cast the proxy client to the IOrganizationService interface.
+                return conn.OrganizationWebProxyClient != null ? conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while connecting to CRM " + ex.Message);
+                Console.ReadKey();
+                return null;
+            }
+        }
+
+        public static ITracingService GetTracingService()
+        {
+            return new FakeTracingService();
+        }
+
+        public static ITracingService GetTracingService(string fileToWriteTracelogs)
+        {
+            isFileTraceLogOn = true;
+            filePath = Path.GetFullPath(fileToWriteTracelogs + ".txt");
+            return new FakeTracingService(filePath);
+        }
+
+        public static OrganizationResponse CallAction(IOrganizationService service, string actionName, EntityReference target, Dictionary<string, object> parameters = null)
+        {
+            var request = new OrganizationRequest(actionName);
+
+            if (!(parameters == null))
+            {
+                parameters.ToList().ForEach(kv =>
+                {
+                    request.Parameters.Add(kv.Key, kv.Value);
+                });
+            }
+
+            request.Parameters.Add("Target", target);
+
+            var response = service.Execute(request);
+            return response;
+        }
+
+        static void OnProcessExit(object sender, EventArgs e)
+        {
+            if (isFileTraceLogOn)
+            {
+                Console.WriteLine($"Your tracing logs has been written to {filePath}");
+                Process.Start(filePath);
+            }
+        }
+    }
+}
---
+++ Dynamics365ConsoleCaller/Dynamics365ConsoleCaller/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dynamics365ConsoleCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IOrganizationService orgService = Connect.GetOrganizationService(
+                 "<UserID>@<OrganizationName>.onmicrosoft.com",
+                 "<Your Crush's Name i.e. Your Password>",
+                 "<CRM login URI>");
+            Console.WriteLine("Connected to Organization Service!");
+
+            ITracingService tracingService = Connect.GetTracingService("AshV_Log");
+
+            // Write Your Testing Code here.
+        }
+    }
+}
