81255fafec9cec825765ae92a6afea88fb74288a
Ashish Vishwakarma <Hello@AshishVishwakarma.com>
2022-02-12 21:29:39 +0530

    updated to latest

--- D365CC/Program.cs
+++ D365CC/Program.cs
@@ -9,18 +9,11 @@ namespace Dynamics365ConsoleCaller
         static void Main(string[] args)
         {
             IOrganizationService orgService;
-            // Option 1 : Connect using Client Secret
             orgService = Connect.GetOrganizationServiceClientSecret(
                  "<Client Id obtained from Azure AD App>",
                  "<Client Secret obtained from Azure AD App>",
                  "<Organization Uri>");
-            /*
-            // Option 2 : Connect using Password
-            IOrganizationService orgService = Connect.GetOrganizationService(
-                 "<UserID>@<OrganizationName>.onmicrosoft.com",
-                 "<Your Crush's Name i.e. Your Password>",
-                 "<CRM login URI>");
-            */
+
             Console.WriteLine("Connected to Organization Service!");
 
             ITracingService tracingService = Connect.GetTracingService("AshV_Log");
